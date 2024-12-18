de26549dc79a9eca0ef2dd9619b3a71a33393ff0
KBismark <bismark.yamoah@amalitech.com>
2024-12-18 11:39:58 +0000

    Add root component

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { QuestionSectionComponent } from "./question-section/question-section.component";
+import { AnswerSectionComponent } from "./answer-section/answer-section.component";
+import { CommonModule } from '@angular/common';
+import { QuizHeaderComponent } from "./quiz-header/quiz-header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, QuestionSectionComponent, AnswerSectionComponent, QuizHeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
